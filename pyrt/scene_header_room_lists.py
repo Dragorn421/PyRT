577002452bbb7179df3aff5a536294b24d41ba8a
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-10-31 17:26:36 +0100

    Reuse find-alternate-headers code

--- pyrt/scene_header_room_lists.py
+++ pyrt/scene_header_room_lists.py
@@ -38,66 +38,77 @@ scene_header_command_struct = struct.Struct(">BBxxI")
 assert scene_header_command_struct.size == 8
 
 
+def find_alternate_headers(scene_data):
+    header_offsets = []
+    offset = 0
+    code = None
+    while code != 0x14:
+        (code, data1, data2) = scene_header_command_struct.unpack_from(
+            scene_data, offset
+        )
+        offset += scene_header_command_struct.size
+        if code != 0x18:
+            continue
+        assert (data2 >> 24) == 0x02
+        alternate_headers_list_offset = data2 & 0xFFFFFF
+        print(
+            "alternate_headers_list_offset = {:06X}".format(
+                alternate_headers_list_offset
+            )
+        )
+        while alternate_headers_list_offset + u32_struct.size <= len(scene_data):
+            (alternate_header_segment_offset,) = u32_struct.unpack_from(
+                scene_data,
+                alternate_headers_list_offset,
+            )
+            alternate_headers_list_offset += u32_struct.size
+            if alternate_header_segment_offset == 0:
+                continue
+            if (alternate_header_segment_offset >> 24) != 0x02:
+                break
+            alternate_header_offset = alternate_header_segment_offset & 0xFFFFFF
+            # check if the data at the offset looks like headers
+            alt_offset = alternate_header_offset
+            alt_code = None
+            while (
+                alt_code != 0x14
+                and alt_offset + scene_header_command_struct.size <= len(scene_data)
+            ):
+                (
+                    alt_code,
+                    alt_data1,
+                    alt_data2,
+                ) = scene_header_command_struct.unpack_from(scene_data, alt_offset)
+                alt_offset += scene_header_command_struct.size
+                # invalid command
+                if alt_code >= 0x1A:
+                    break
+                # invalid commands in the context of alternate scene headers
+                if alt_code in {0x01, 0x0A, 0x0B, 0x18}:
+                    break
+                # valid commands if data2 is a valid segment offset
+                # (the segment is always the scene file)
+                if alt_code in {0x00, 0x03, 0x04, 0x06, 0x0E, 0x0F, 0x13}:
+                    if (data2 >> 24) != 0x02:
+                        break
+                    if (data2 & 0xFFFFFF) >= len(scene_data):
+                        break
+            if alt_code == 0x14:
+                header_offsets.append(alternate_header_offset)
+            else:
+                break
+    return header_offsets
+
+
 def parse_scene_headers(
     pyrti,  # type: pyrt.PyRTInterface
-    ):
+):
     rom = pyrti.rom
 
     rooms_by_scene = dict()  # type: Dict[pyrt.SceneTableEntry, List[pyrt.RomFile]]
     for scene_table_entry in rom.scene_table:
         scene_data = scene_table_entry.scene_file.data
-        header_offsets = [0]
-        # find alternate headers
-        offset = 0
-        code = None
-        while code != 0x14:
-            (code, data1, data2) = scene_header_command_struct.unpack_from(
-                scene_data, offset
-            )
-            offset += scene_header_command_struct.size
-            if code != 0x18:
-                continue
-            alternate_headers_list_offset = data2
-            while alternate_headers_list_offset + u32_struct.size <= len(scene_data):
-                (alternate_header_segment_offset,) = u32_struct.unpack_from(
-                    scene_data,
-                    alternate_headers_list_offset,
-                )
-                alternate_headers_list_offset += u32_struct.size
-                if alternate_header_segment_offset == 0:
-                    continue
-                if (alternate_header_segment_offset >> 24) != 0x02:
-                    break
-                alternate_header_offset = alternate_header_segment_offset & 0xFFFFFF
-                # check if the data at the offset looks like headers
-                alt_offset = alternate_header_offset
-                alt_code = None
-                while (
-                    alt_code != 0x14
-                    and alt_offset + scene_header_command_struct.size <= len(scene_data)
-                ):
-                    (
-                        alt_code,
-                        alt_data1,
-                        alt_data2,
-                    ) = scene_header_command_struct.unpack_from(scene_data, alt_offset)
-                    # invalid command
-                    if alt_code >= 0x1A:
-                        break
-                    # invalid commands in the context of alternate scene headers
-                    if alt_code in {0x01, 0x0A, 0x0B, 0x18}:
-                        break
-                    # valid commands if data2 is a valid segment offset
-                    # (the segment is always the scene file)
-                    if alt_code in {0x00, 0x03, 0x04, 0x06, 0x0E, 0x0F, 0x13}:
-                        if (data2 >> 24) != 0x02:
-                            break
-                        if (data2 & 0xFFFFFF) >= len(scene_data):
-                            break
-                if alt_code == 0x14:
-                    header_offsets.append(alternate_header_offset)
-                else:
-                    break
+        header_offsets = [0] + find_alternate_headers(scene_data)
         # find room list from all headers
         for offset in header_offsets:
             code = None
@@ -148,75 +159,10 @@ def pack_room_lists(
 ):
     rom = pyrti.rom
 
-    # TODO mostly copypasted from parse_scene_headers, make code common
     for scene_table_entry, room_list in rom.rooms_by_scene.items():
         print("Scene ", scene_table_entry.scene_file.dma_entry)
         scene_data = bytearray(scene_table_entry.scene_file.data)
-        header_offsets = [0]
-        # find alternate headers
-        offset = 0
-        code = None
-        while code != 0x14:
-            (code, data1, data2) = scene_header_command_struct.unpack_from(
-                scene_data, offset
-            )
-            offset += scene_header_command_struct.size
-            if code != 0x18:
-                continue
-            # FIXME there are fixes like this line not present in the og code that got copypasted
-            # TODO check segment 2
-            alternate_headers_list_offset = data2 & 0xFFFFFF
-            import sys
-
-            print(
-                "alternate_headers_list_offset = {:06X}".format(
-                    alternate_headers_list_offset
-                ),
-                file=sys.stderr,
-            )
-            while alternate_headers_list_offset + u32_struct.size <= len(scene_data):
-                (alternate_header_segment_offset,) = u32_struct.unpack_from(
-                    scene_data,
-                    alternate_headers_list_offset,
-                )
-                alternate_headers_list_offset += u32_struct.size
-                if alternate_header_segment_offset == 0:
-                    continue
-                if (alternate_header_segment_offset >> 24) != 0x02:
-                    break
-                alternate_header_offset = alternate_header_segment_offset & 0xFFFFFF
-                # check if the data at the offset looks like headers
-                alt_offset = alternate_header_offset
-                alt_code = None
-                while (
-                    alt_code != 0x14
-                    and alt_offset + scene_header_command_struct.size <= len(scene_data)
-                ):
-                    (
-                        alt_code,
-                        alt_data1,
-                        alt_data2,
-                    ) = scene_header_command_struct.unpack_from(scene_data, alt_offset)
-                    alt_offset += (
-                        scene_header_command_struct.size
-                    )  # FIXME another uncopied bugfix
-                    # invalid command
-                    if alt_code >= 0x1A:
-                        break
-                    # invalid commands in the context of alternate scene headers
-                    if alt_code in {0x01, 0x0A, 0x0B, 0x18}:
-                        break
-                    # valid commands if data2 is a valid segment offset
-                    # (the segment is always the scene file)
-                    if alt_code in {0x00, 0x03, 0x04, 0x06, 0x0E, 0x0F, 0x13}:
-                        if (data2 >> 24) != 0x02:
-                            break
-                        if (data2 & 0xFFFFFF) >= len(scene_data):
-                            break
-                if alt_code == 0x14:
-                    header_offsets.append(alternate_header_offset)
-                else:
-                    break
+        header_offsets = [0] + find_alternate_headers(scene_data)
         # find room list from all headers
         for offset in header_offsets:
             print("  Header 0x{:06X}".format(offset))
